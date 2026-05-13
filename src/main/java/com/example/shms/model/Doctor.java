5a8b7d84fc496e66e41ec533413f58b21afffd34
Mariam ElShafie <m.a.elshafei2007@gmail.com>
2026-05-13 13:29:45 +0300

    doc part 1

---
+++ src/main/java/com/example/shms/model/Doctor.java
@@ -0,0 +1,11 @@
+package com.example.shms.model;
+
+public class Doctor {
+    private int ID;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+
+}
