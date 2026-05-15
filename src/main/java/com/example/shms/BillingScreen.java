b7918fd72a0d84be89de42b6582fd4de84e669c6
salma-abdelkhalek <salmaabdelkhalek26@gmail.com>
2026-05-15 15:15:47 +0300

    update added department to patients

--- src/main/java/com/example/shms/BillingScreen.java
+++ src/main/java/com/example/shms/BillingScreen.java
@@ -5,6 +5,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class BillingScreen extends Application {
+
+    private TableView<Bill> billingTable;
+    private ObservableList<Bill> allBills;
+
+    @java.lang.Override
+    public void start(Stage stage)  {
+
+    }
 }
