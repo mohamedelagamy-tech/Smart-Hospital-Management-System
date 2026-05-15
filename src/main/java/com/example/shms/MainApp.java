49151c8477d0c4536c6c1e07bb51d0ff8b63c203
Mohamed Mazen <m.mazenelagamy@gmail.com>
2026-05-15 12:20:08 +0300

    reverted MainApp back to PatientView

--- src/main/java/com/example/shms/MainApp.java
+++ src/main/java/com/example/shms/MainApp.java
@@ -3,35 +3,19 @@ package com.example.shms;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class MainApp extends Application {
-
-    private static Stage primaryStage;
 
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage stage) throws Exception {
-        primaryStage = stage;
-        navigateTo("login.fxml",800,500);
-        stage.setTitle("Hospital Mangement System");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/shms/fxml/PatientView.fxml"));
+        Scene scene = new Scene(loader.load(),900,600);
+        stage.setTitle("Hospital management system");
+        stage.setScene(scene);
         stage.show();
-    }
-
-    public static void navigateTo(String fxmlFile, int width, int height) {
-        try {
-            FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/com/example/shms/fxml/" + fxmlFile));
-            Pane root = loader.load();
-            Scene scene = new Scene(root,width,height);
-            primaryStage.setScene(scene);
-        } catch (IOException e) {
-            System.out.println("Navigation failed: "+e.getMessage());
-        }
     }
 }
