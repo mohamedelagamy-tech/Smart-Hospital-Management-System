a48dc158322a2a52aec4e18e0e3433eabf9dfcd2
salma-abdelkhalek <salmaabdelkhalek26@gmail.com>
2026-05-15 14:10:38 +0300

    update added department to patients

--- src/main/java/com/example/shms/model/Bill.java
+++ src/main/java/com/example/shms/model/Bill.java
@@ -1,7 +1,7 @@
 package com.example.shms.model;
 public class Bill implements Billable {
 
-    private int billNumber;
+    private final int billNumber;
     private int patientId;
     private int appointmentId;
     private double amount;
@@ -18,10 +18,11 @@ public class Bill implements Billable {
     public int getPatientId() { return patientId; }
     public int getAppointmentId() { return appointmentId; }
     public double getAmount() { return amount; }
-    public String getStatus() { return status; }
+    public String getBillStatus() { return status; }
     public String getBILLDate() { return date; }
 
-    public void setStatus(String status) { this.status = status; }
+
+    public void setBillStatus(String status) { this.status = status; }
     public void setAmount(double amount) { this.amount = amount; }
 
 }
