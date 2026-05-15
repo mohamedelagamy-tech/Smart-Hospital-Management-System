23289c1bba34ff30141f568a9a025d1a6d30a792
zeina <Zeinafarghaly@gmail.com>
2026-05-15 16:28:00 +0300

    correction

--- src/main/java/com/example/shms/model/DischargeSummary.java
+++ src/main/java/com/example/shms/model/DischargeSummary.java
@@ -2,31 +2,30 @@
 package com.example.shms.model;
 import java.util.List;
 import java.io.FileWriter;
-import java.io.IOExeption;
-public class DisshargeSummary {
+import java.io.IOException;
+public class DischargeSummary {
 
     private final int patientID;
     private final int doctorID;
     private int roomUsed;
-    private List<prescription> prescriptions;
+    private List<Prescription> prescriptions;
     private double totalBill;
 
-    public DisshargeSummary(int doctorID, int patientID, List<prescription> prescriptions, int roomUsed, double tatalBill) {
+    public DischargeSummary(int doctorID, int patientID, List<Prescription> prescriptions, int roomUsed, double totalBill) {
         this.doctorID = doctorID;
         this.patientID = patientID;
         this.prescriptions = prescriptions;
         this.roomUsed = roomUsed;
-        this.tatalBill = tatalBill;
+        this.totalBill = totalBill;
     }
     public int getPatientID() {
         return patientID;
     }
     public double getTotalBill() {
-        return tatalBill;
+        return totalBill;
     }
 public void writeToFile() {
-    try {
-        FileWriter writer = new FileWriter("discharge_" + patientID + ".txt");
+    try(FileWriter writer = new FileWriter("discharge_" + patientID + ".txt")) {
         writer.write("=== DISCHARGE SUMMARY ===\n");
         writer.write("Patient ID: " + patientID + "\n");
         writer.write("Doctor ID: " + doctorID + "\n");
@@ -36,11 +35,10 @@ public void writeToFile() {
         for (Prescription p : prescriptions) {
             writer.write(" -" + p.getMedicineName() + "|" + p.getDosage() + "|" + p.getDuration() + "\n");
         }
-        writer.close();
         System.out.println("file saved!");
         }
     catch (IOException e){
-        System.out.println("Error writing file");
+        System.out.println("Error writing file"+e.getMessage());
     }
 }
     
