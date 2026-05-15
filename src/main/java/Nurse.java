471116c655aea6aa084564d55dec050ca57b9d6e
Mariam ElShafie <m.a.elshafei2007@gmail.com>
2026-05-15 09:31:47 +0300

    nurse part 1

--- src/main/java/Nurse.java
+++ src/main/java/Nurse.java
@@ -1,5 +1,8 @@
 package com.example.shms.model;
 
-public class Nurse {
-
+public class Nurse extends Person {
+    private String department;
+    private String status;
+    private double totalRating;
+    private int ratingCount;
 }
