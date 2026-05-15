77b47ee3650d580bc35570894d80aa4331807b6c
salma-abdelkhalek <salmaabdelkhalek26@gmail.com>
2026-05-15 14:54:47 +0300

    update added department to patients

---
+++ src/main/java/com/example/shms/BillingScreen.java
@@ -0,0 +1,10 @@
+package com.example.shms;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+
+public class BillingScreen extends Application {
+}
